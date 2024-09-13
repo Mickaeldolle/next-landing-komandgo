a97bea48d18349a09984cb74a873b96803e1e9db
Mickaeldolle <dolle.mickael@gmail.com>
2024-09-13 11:56:40 +0200

    fix error in auth route

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,11 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import NextAuth, { Account, NextAuthOptions, Session as NextAuthSession } from "next-auth";
-
-interface Session extends NextAuthSession {
-  accessToken?: string;
-}
+import NextAuth, { NextAuthOptions, Session } from "next-auth";
 import { JWT } from "next-auth/jwt";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
-export const authOptions: NextAuthOptions = {
+const authOptions: NextAuthOptions = {
   session : {
     strategy: 'jwt',
   },
@@ -17,27 +13,10 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-      // authorization: {
-      //   params: {
-      //     scope: "openid email profile https://www.googleapis.com/auth/user.phonenumbers.read",
-      //   },
-      // },
+      
     }),
   ],
   callbacks: {
-    // async jwt({ token, account }: { token: JWT; account: Account | null }) {
-    //   if (account) {
-    //     token.accessToken = account.access_token;
-    //   }
-    //   return token;
-    // },
-
-    // async session({ session, token }) {
-    //   // Associer l'accessToken à la session utilisateur
-    //   (session as Session).accessToken = token.accessToken as string;
-    //   return session;
-    // },
-
     async signIn({ account, profile }) {
       if (!profile?.email) {
         throw new Error("No email returned from Google");
